92c0152e9961e11242695891a2908242fbe478f2
Park Hanee <hanea8199@gmail.com>
2017-08-03 16:53:00 +0900

    친구/대화 프래그먼트에 탭 적용

--- app/src/main/java/com/parkhanee/tinychat/MainActivity.java
+++ app/src/main/java/com/parkhanee/tinychat/MainActivity.java
@@ -1,9 +1,16 @@
 package com.parkhanee.tinychat;
 
+
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,51 +19,82 @@ import com.parkhanee.tinychat.classbox.Friend;
 import com.parkhanee.tinychat.classbox.Room;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     MyPreferences pref=null;
     Context context=this;
     MySQLite mySQLite = null;
-    TextView logout;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         if (pref==null){ pref = MyPreferences.getInstance(context); }
         // TODO: 2017. 8. 2. sqlite getInstance를 IntentService로 ..?
         if (mySQLite==null){ mySQLite = MySQLite.getInstance(context); }
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        // TODO: 2017. 8. 1.   로그아웃 버튼과 이름보이는 텍스트뷰 분리 해야함
-        logout = (TextView) findViewById(R.id.logout);
-        logout.setText("name "+pref.getString("name"));
-        logout.setOnClickListener(this);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TalkTabFragment(), "TALK");
+        adapter.addFragment(new FriendTabFragment(), "FRIEND");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.logout : // 로그아웃
-
-//                ArrayList<Room> r = mySQLite.getAllRooms();
-//                logout.setText(r.toString());
-
-//                Friend f = mySQLite.getFriend("12341234");
-//                logout.setText(f.toString());
-//                mySQLite.getAllFriends();
-
-                if (pref.logout()){
-                    // TODO: 2017. 7. 27. 로그아웃 되었습니다 알림
-                    Toast.makeText(context, "로그아웃 되었습니다", Toast.LENGTH_SHORT).show();
-                    Intent i = new Intent(context,LoginActivity.class);
-                    startActivity(i);
-                    finish();
-                    return;
-                }
-                // TODO: 2017. 7. 29. 한번 로그아웃 누르고 화면 넘어가기 전에 누르면 이쪽으로 옴. 아예 안눌리도록 처리 해야.
-                Toast.makeText(context, "SP 로그아웃 실패??", Toast.LENGTH_SHORT).show();
-                break;
+//            case R.id.logout : // 로그아웃
+//
+//
+//                break;
 
         }
     }
