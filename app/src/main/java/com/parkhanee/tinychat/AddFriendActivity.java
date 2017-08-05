114ddf7a8f2027b082ce4571202ad8d9e9fd24f6
Park Hanee <hanea8199@gmail.com>
2017-08-05 16:43:40 +0900

    AddFriendActivity의 toolbar에 뒤로 버튼 추가, 색상 조정

--- app/src/main/java/com/parkhanee/tinychat/AddFriendActivity.java
+++ app/src/main/java/com/parkhanee/tinychat/AddFriendActivity.java
@@ -1,7 +1,10 @@
 package com.parkhanee.tinychat;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class AddFriendActivity extends AppCompatActivity {
 
@@ -9,6 +12,14 @@ public class AddFriendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false); // do not show default name text and instead, show the textView i included
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // do not show back button
+
+//        getSupportActionBar().setHomeAsUpIndicator(getResources().getDrawable(R.drawable.back));
+
 
 
 
